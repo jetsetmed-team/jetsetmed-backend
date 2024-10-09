3a93b870b08890c3efd249b322eeb70871a35b18
Ishan Audichya <audichyaishan@gmail.com>
2024-10-10 02:03:52 +0530

    minor bug

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -190,7 +190,7 @@ exports.forgotPassword = async (req, res) => {
       subject: "Password Reset",
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
-        ${appUrl}/resetPassword/${token}\n\n
+        ${appUrl}/reset-password/${token}\n\n
         If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
